1670b6f9dd8f6f9afa80360141a784f3a6fa121a
g4esp <gianniesp@outlook.com>
2022-03-27 21:31:16 +0200

    fix some bug

--- src/Me.jsx
+++ src/Me.jsx
@@ -11,7 +11,9 @@ export const G4ESP = () => {
   const [me, setMe] = useState()
   
   useEffect(() => {
-    fetch('https://github.com/g4esp/g4esp-cv/blob/main/src/g4esp.json')
+    fetch('src/g4esp.json',{
+      mode: "no-cors" // 'cors' by default
+    })
         .then((data) => data.json())
         .then((profile) => setMe(profile))
         .catch((err) => console.log(err.message))
